bb569d7d25c420b5b1cbac14d375b889b42839d6
blackLearning <fuyigekua@foxmail.com>
2017-12-07 23:53:18 +0800

    Add redux-thunk for simple side-effect

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import storage from 'redux-persist/lib/storage'
 import createSagaMiddleware, { END } from 'redux-saga';
 import Immutable from 'seamless-immutable';
-// import ReduxThunk from 'redux-thunk';
+import reduxThunk from 'redux-thunk';
 import { persistReducer, persistStore, createTransform } from 'redux-persist';
 import rootReducer from '../reducer';
 import enhancers from './enhancers';
@@ -11,7 +11,7 @@ import screenTrackingMiddleware from './screenTrackingMiddleware';
 // Note: logger must be the last middleware in chain,
 // otherwise it will log thunk and promise, not actual actions
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = [screenTrackingMiddleware, sagaMiddleware];
+const middlewares = [reduxThunk, screenTrackingMiddleware, sagaMiddleware];
 if (__DEV__) {
   const { logger } = require('redux-logger'); // eslint-disable-line
   middlewares.push(logger);
@@ -19,9 +19,9 @@ if (__DEV__) {
 
 const myTransform = createTransform(
   // transform state coming from storage, on its way to be rehydrated into redux
-  state => (Immutable.isImmutable(state) ? Immutable.asMutable(state) : state),
-  // transform state coming from redux on its way to being serialized and stored
   state => Immutable(state),
+  // transform state coming from redux on its way to being serialized and stored
+  state => (Immutable.isImmutable(state) ? Immutable.asMutable(state) : state),
 )
 
 const config = {
